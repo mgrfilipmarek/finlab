8209b44c29a3610e681c2ce68a4768fa0ba89175
filip <mgr.filipmarek@gmail.com>
2018-11-06 12:46:26 +0100

    routing between components

--- frontend/src/main/frontend/src/app/app-routing.module.ts
+++ frontend/src/main/frontend/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from "@angular/router";
 import {PaymentsComponent} from "./payments/payments.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'payments', component: PaymentsComponent }
 ];
 
