723e0e797b9962a3ba13b3b8c91384d42341ff02
filip <mgr.filipmarek@gmail.com>
2018-11-06 11:36:17 +0100

    userDetail component

---
+++ backend/src/test/java/finlab/MockMvcWebTest.java
@@ -0,0 +1,37 @@
+package finlab;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class MockMvcWebTest {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
