51251425a8b84d0bfd73df3b51a9c63368ffef7f
filip <mgr.filipmarek@gmail.com>
2018-11-20 09:52:39 +0100

    doplneny repo/controller na last payments

--- backend/src/main/java/finlab/controller/PaymentController.java
+++ backend/src/main/java/finlab/controller/PaymentController.java
@@ -1,10 +1,14 @@
 package finlab.controller;
 
+import finlab.data.Payment;
 import finlab.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -17,6 +21,14 @@ public class PaymentController {
     @RequestMapping("/all")
     public List paymentAll() {
         return repo.findAll();
+    }
+
+    @RequestMapping("/last")
+    public List<Payment> lastPayments(@RequestParam int days) {
+//        return repo.findLastDays(days);
+        LocalDate ld = LocalDate.now();
+        ld = ld.minusDays((long)days);
+        return repo.findAllByPaymentTimeAfter(Date.valueOf(ld));
     }
 
 }
--- backend/src/main/java/finlab/repository/PaymentRepository.java
+++ backend/src/main/java/finlab/repository/PaymentRepository.java
@@ -1,12 +1,21 @@
 package finlab.repository;
 
 import finlab.data.Payment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PaymentRepository extends CrudRepository<Payment, Integer> {
 
     List<Payment> findAll();
+
+    @Query(value = "SELECT * FROM payment as p " +
+            "WHERE 1=1 " +
+            "AND p.payment_time >= date(now()) - ?1", nativeQuery = true)
+    List<Payment> findLastDays(int d);
+
+    List<Payment> findAllByPaymentTimeAfter(Date d);
 
 }
